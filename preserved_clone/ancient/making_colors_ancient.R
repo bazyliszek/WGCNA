117a14d0f11124e55e84694fc469442573f63e9e
Marcin <bazyliszek@users.noreply.github.com>
2019-03-17 21:23:49 +0100

    Preserved_clone

---
+++ preserved_clone/ancient/making_colors_ancient.R
@@ -0,0 +1,5 @@
+load("mergedColors_Part4_ancient.RData")
+mergedColors_ancient <- mergedColors
+unique(mergedColors)
+save(mergedColors_ancient, file="mergedColors_ancient.RData")
+
---
+++ preserved_clone/recent/making_colorors_recent.R
@@ -0,0 +1,5 @@
+load("mergedColors_Part4_recent.RData")
+mergedColors_recent <- mergedColors
+unique(mergedColors)
+save(mergedColors_recent, file="mergedColors_recent.RData")
+
